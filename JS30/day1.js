93c368e16de3e738bca33635c808dcbe71a6347d
Dikshant Solanki <dcode2100@gmail.com>
2023-04-29 12:06:01 +0530

    day 9 and revision

--- JS30/day1.js
+++ JS30/day1.js
@@ -3,10 +3,11 @@
 // 
 //Q1. Write a function that takes a string as an argument and returns true if it's a palindrome, false otherwise.
 
-/*
+
 function palindromeChecker() {
-    var str = [3,4,5,1,2,5,4,3];
+    var str = [3,4,5,6,6,5,4,3];
     var i = 0,j = str.length-1;
+    console.log(str[0]);
     var ans = true;
     for (let k = 0; k < str.length/2; k++) {
         if(str[i] !== str[j]) {
@@ -20,6 +21,6 @@ function palindromeChecker() {
 }
 
 palindromeChecker();
-*/
+
 
 
